d6638eaacbeac545b5fdd7a10e2abe33b0368bfd
Pablo Postigo <pablo@worona.org>
2017-01-23 11:11:17 +0100

    Fixing translate

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -2,8 +2,11 @@ import * as components from './components';
 import * as sagas from './sagas';
 import * as deps from './deps';
 
+const locales = lang => require(`./locales/${lang}.json`);
+
 export {
   components,
+  locales,
   sagas,
   deps,
 };
